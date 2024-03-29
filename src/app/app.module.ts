1d9f6c2e9e43c5fc4be2f64923fe6091a569f24d
Jen <jenifferprc@gmail.com>
2024-03-29 18:08:48 +0100

    Add translateModule logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from '../profile/profile.component';
 import { ProfileImgComponent } from '../profile/components/profile-img/profile-img.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent, ProfileComponent, ProfileImgComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.get('profile').subscribe((res) => {
+      console.log(res);
+    });
+  }
 
+  changeLocale(locale: string) {
+    this.translate.use(locale);
+  }
 }
