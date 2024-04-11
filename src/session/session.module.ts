ac01fa902df74cd1da380087fed4041644df2aa3
Jen <jenifferprc@gmail.com>
2024-04-11 21:39:35 +0200

    Add empty search component

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionComponent } from './session.component';
+import { SearchComponent } from '../session/components/search/search.component';
 
 @NgModule({
-  declarations: [SessionComponent],
+  declarations: [SessionComponent, SearchComponent],
   imports: [CommonModule],
   exports: [SessionComponent],
 })
